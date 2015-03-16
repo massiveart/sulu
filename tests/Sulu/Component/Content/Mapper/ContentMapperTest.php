a8a3807e0d3be1bbdf4defd937ae19e5e084c2a0
Johannes Wachter <johannes.wachter@massiveart.at>
2015-03-16 16:34:45 +0100

    fixes #883

--- tests/Sulu/Component/Content/Mapper/ContentMapperTest.php
+++ tests/Sulu/Component/Content/Mapper/ContentMapperTest.php
@@ -2299,6 +2299,60 @@ class ContentMapperTest extends PhpcrTestCase
         $this->assertEquals('/page-1', $result->url);
     }
 
+    private function prepareCopyLanguageTree()
+    {
+        $this->mapper->saveStartPage(array('title' => 'Start Page'), 'overview', 'default', 'de', 1);
+        $this->mapper->saveStartPage(array('title' => 'Start Page'), 'overview', 'default', 'en', 1);
+
+        $data = array(
+            array(
+                'title' => 'test',
+                'url' => '/test'
+            ),
+            array(
+                'title' => 'childtest',
+                'url' => '/test/childtest'
+            )
+        );
+
+        $data[0] = $this->mapper->save($data[0], 'overview', 'default', 'de', 1);
+        $data[1] = $this->mapper->save($data[1], 'overview', 'default', 'de', 1, true, null, $data[0]->getUuid());
+
+        return $data;
+    }
+
+    public function testCopyLanguageTree()
+    {
+        $data = $this->prepareCopyLanguageTree();
+
+        $this->mapper->copyLanguage($data[0]->getUuid(), 1, 'default', 'de', 'en');
+        $this->mapper->save(
+            array('title' => 'test-en', 'url' => '/test-en'),
+            'overview',
+            'default',
+            'en',
+            1,
+            true,
+            $data[0]->getUuid(),
+            null,
+            null,
+            false
+        );
+
+        $this->session->refresh(false);
+
+        $this->mapper->copyLanguage($data[1]->getUuid(), 1, 'default', 'de', 'en');
+
+
+        $result = $this->mapper->load($data[0]->getUuid(), 'default', 'en');
+        $this->assertEquals('test-en', $result->getPropertyValue('title'));
+        $this->assertEquals('/test-en', $result->getPropertyValue('url'));
+
+        $result = $this->mapper->load($data[1]->getUuid(), 'default', 'en');
+        $this->assertEquals('childtest', $result->getPropertyValue('title'));
+        $this->assertEquals('/test-en/childtest', $result->getPropertyValue('url'));
+    }
+
     private function checkTreeResult($result)
     {
         // layer 0
@@ -2885,7 +2939,8 @@ class ContentMapperTest extends PhpcrTestCase
     /**
      * @return StructureInterface[]
      */
-    private function prepareOrderAtData() {
+    private function prepareOrderAtData()
+    {
         $data = array(
             array(
                 'title' => 'Page-1',
@@ -3185,7 +3240,8 @@ class ContentMapperTest extends PhpcrTestCase
         $this->assertEquals('/page-2/sub-1', $result[2]->getPath());
     }
 
-    public function testOrderAt() {
+    public function testOrderAt()
+    {
         $data = $this->prepareOrderAtData();
 
         $result = $this->mapper->orderAt($data[2]->getUuid(), 3, 17, 'default', 'en');
@@ -3200,7 +3256,8 @@ class ContentMapperTest extends PhpcrTestCase
         $this->assertEquals('/page-1/page-1-4', $result[3]->getPath());
     }
 
-    public function testOrderAtToLast() {
+    public function testOrderAtToLast()
+    {
         $data = $this->prepareOrderAtData();
 
         $result = $this->mapper->orderAt($data[2]->getUuid(), 4, 1, 'default', 'en');
